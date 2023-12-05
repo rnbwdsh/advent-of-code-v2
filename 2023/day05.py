5fd3cb062b6db768c175727a46edcc5a10271af8
m.vogl <markus.vogl@rise-world.com>
2023-12-05 07:47:01 +0100

    Day 5 + draft of 6

--- 2023/day05.py
+++ 2023/day05.py
@@ -1,32 +1,83 @@
-import re
-from collections import Counter
-from typing import List
+from typing import List, Optional
 
 import pytest
 
-@pytest.mark.data("""Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""", 13, 30)
-def test_05(data: List[str], level):
+
+@pytest.mark.data("""seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4""", 35, 46)
+def test_05(data: List[List[str]], level):
     total = 0
-    ctr = Counter()
-    for line in data:
-        num, left, right = re.search( r'Card +(\d+):([\d ]+)\|([\d ]+)', line.replace("  ", " ")).groups()
-        left  = set([int(l) for l in  left.strip().split(" ")])
-        right = set([int(r) for r in right.strip().split(" ")])
-        nr_match = len(left & right)
-        if nr_match:
-            total += 1 << (nr_match - 1)
-        ctr[int(num)] = nr_match
+    seeds = [int(i) for i in data[0][0].split(": ")[1].split(" ")]
     if level:
-        max_id = max(ctr.keys())
-        ticket_num = Counter(ctr.keys())
-        for src in range(max_id):
-            ticket_num += {to_add+1: ticket_num[src]
-                           for to_add in range(src, src + ctr[src])
-                           if to_add < max_id}
-        return sum(ticket_num.values())
-    return total
+        seeds = [range(a, a+b) for a, b in zip(seeds[::2], seeds[1::2])]
+    else:
+        seeds = [range(s, s+1) for s in seeds]
+    for chunk in data[1:]:
+        mapper = {}
+        for line in chunk[1:]:
+            dst, src, ran = [int(i) for i in line.split(" ")]
+            mapper[range(src, src+ran)] = range(dst, dst+ran)
+        seeds = map_ranges(seeds, mapper)
+    return min([s.start for s in seeds])
+
+
+def range_(start: int, stop: int) -> Optional[range]:
+    if start >= stop:
+        return None
+    return range(start, stop)
+
+def range_intersection(seed: range, to_match: range) -> (Optional[range], Optional[range], Optional[range]):
+    start = max(seed.start, to_match.start)
+    stop = min(seed.stop, to_match.stop)
+    return range_(start, stop)
+
+def map_ranges(seeds, range_mapping):
+    mapped_ranges = set()
+    while seeds:
+        seed_range = seeds.pop()
+        if seed_range is None:
+            continue
+        for src_range, target_range in range_mapping.items():
+            intersect = range_intersection(seed_range, src_range)
+            if intersect:
+                length = intersect.start - src_range.start
+                start = target_range.start + length
+                mapped_range = range_(start, start + len(intersect))
+                mapped_ranges.add(mapped_range)
+                seeds.append(range_(seed_range.start, intersect.start))
+                seeds.append(range_(intersect.stop, seed_range.stop))
+                break
+        else:
+            mapped_ranges.add(seed_range)
+    return list(mapped_ranges)
---
+++ 2023/day06.py
@@ -0,0 +1,32 @@
+import re
+from collections import Counter
+from typing import List
+
+import pytest
+
+@pytest.mark.data("""Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""", 13, 30)
+def test_06(data: List[str], level):
+    total = 0
+    ctr = Counter()
+    for line in data:
+        num, left, right = re.search( r'Card +(\d+):([\d ]+)\|([\d ]+)', line.replace("  ", " ")).groups()
+        left  = set([int(l) for l in  left.strip().split(" ")])
+        right = set([int(r) for r in right.strip().split(" ")])
+        nr_match = len(left & right)
+        if nr_match:
+            total += 1 << (nr_match - 1)
+        ctr[int(num)] = nr_match
+    if level:
+        max_id = max(ctr.keys())
+        ticket_num = Counter(ctr.keys())
+        for src in range(max_id):
+            ticket_num += {to_add+1: ticket_num[src]
+                           for to_add in range(src, src + ctr[src])
+                           if to_add < max_id}
+        return sum(ticket_num.values())
+    return total
