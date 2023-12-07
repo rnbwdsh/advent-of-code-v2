612f140fe9c363670b1526fa1b22bfeb73588506
m <vogl91@gmail.com>
2023-12-07 07:45:42 +0100

    Refactor 2020 add current day

--- 2023/day07.py
+++ 2023/day07.py
@@ -1,19 +1,37 @@
-import re
 from collections import Counter
 from typing import List
 
-import numpy as np
 import pytest
 
-@pytest.mark.data("""Time:      7  15   30
-Distance:  9  40  200""", 288, 71503)
+def score_hand_0(hand: List[int]):
+    nr_ctr = dict(Counter(Counter(hand).values()))
+    patterns = [{1: 5}, {2: 1, 1: 3}, {2: 2, 1: 1}, {3: 1, 1: 2}, {2: 1, 3: 1}, {4: 1, 1: 1}, {5: 1}]
+    for score, pattern in enumerate(patterns):
+        if pattern == nr_ctr:
+            return score
+    raise ValueError(f"Unknown hand: {hand} {nr_ctr}")
+
+def permuted(hand: List[int], repl: int):
+    hc = hand.copy()
+    hc[hand.index(0)] = repl
+    return score_hand_1(hc)
+
+def score_hand_1(hand: List[int]):
+    if 0 not in hand:
+        return score_hand_0(hand)
+    return max(permuted(hand, card) for card in range(1, 13))
+
+def parse(line: str, level):
+    hand, bid = line.split(" ")
+    hand = [("J23456789TQKA".index(card) if level else "23456789TJQKA".index(card)) for card in hand]
+    score = score_hand_1(hand) if level else score_hand_0(hand)
+    return score, hand, int(bid)
+
+@pytest.mark.data("""32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483""", 6440, 5905)
 def test_07(data: List[str], level):
-    time, dist = data
-    if level:
-        time = time.replace(" ", "")
-        dist = dist.replace(" ", "")
-    time = [int(i) for i in re.findall("\d+", time)]
-    dist = [int(i) for i in re.findall("\d+", dist)]
-    return np.prod([sum((t-wt) * wt > d
-                        for wt in range(1, t))
-                    for t, d in zip(time, dist)])
+    all_hands = [parse(line, level) for line in data]
+    return sum(bid * (rank + 1) for rank, (_, _, bid) in enumerate(sorted(all_hands)))
