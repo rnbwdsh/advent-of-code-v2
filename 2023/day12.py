e13f1830f6fbc83c25ebbbfd68e23a9c9496ce42
m <vogl91@gmail.com>
2023-12-12 11:17:46 +0100

    Improved logic

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,40 +1,29 @@
-import re
 from functools import cache
 from typing import List, Tuple
 
 import pytest
 
-
 @cache
-def calc_repl(s: str, repl: Tuple[int], in_string=False) -> int:
-    """ Example: #?# -> #.#, ###
-    replaces the first ? with . and then with #, and if there are any more ? in there, it calls itself again """
-    if not s and (not repl or repl == (0,)):
+def calc_repl(s: str, r: Tuple[int], in_string=False) -> int:
+    """ Recursively calculate the number of replacements for a given string and a pattern of replacements """
+    if not s and (not r or r == (0,)):
         return 1
-    if (not repl and "#" in s) or sum(repl) > (s.count("#") + s.count("?")):
+    if (not r and "#" in s) or sum(r) > (s.count("#") + s.count("?")):
         return 0
 
-    sn = s[1:]
-    r0 = repl[0] if repl else 0
-    repl_next = tuple([r0 - 1]) + repl[1:] if repl else (0,)
+    s, sn = s[0], s[1:]  # can't use deconstruct, because that would result in an tuple (un-cache-able)
+    r0 = r[0] if r else 0
 
-    if s[0] == ".":
-        if in_string and r0:        return 0
-        elif in_string and not r0:  return calc_repl(sn, repl[1:], False)
-        elif not in_string:         return calc_repl(sn, repl if r0 else repl[1:], False)
-    elif s[0] == "#":
-        if in_string and r0:        return calc_repl(sn, repl_next, True)
-        elif in_string and not r0:  return 0
-        elif not in_string:         return calc_repl(sn, repl_next if r0 else repl[1:], True)
-    elif s[0] == "?":
-        if in_string and r0:        return calc_repl(sn, repl_next, True)
-        elif in_string and not r0:  return calc_repl(sn, repl[1:], False)
-        elif not in_string and r0:  return calc_repl(sn, repl, False) + calc_repl(sn, repl_next, True)
-        else:                       return calc_repl(sn, repl, False)
-
-def count_continous_hashtags(l):
-    """ Example: ###...#.# -> 3,1,1"""
-    return [len(group) for group in re.findall(r"(#+)", l)]
+    if s == "." and in_string and r0 or s == "#" and not r0:
+        return 0  # recursion root: can't continue with ? or .
+    if s == "?" and not in_string and r0:
+        return calc_repl("." + sn, r, False) + calc_repl("#" + sn, r, False)  # call self but with # and . instead of ?
+    if (s == "#" or s == "?" and in_string) and r0:
+        r = tuple([r0 - 1]) + r[1:]   # reduce the repl[ß] by 1
+    if (s == "." or s == "?" and in_string) and not r0:
+        r = r[1:]  # remove the repl[0] if it's 0
+    in_string = s == "#" or s == "?" and r0  # the next in-string depends on the current char and the repl[0]
+    return calc_repl(sn, r, in_string)
 
 @pytest.mark.data("""???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -44,7 +33,7 @@ def count_continous_hashtags(l):
 ?###???????? 3,2,1""", 21, 525152)
 def test_12(data: List[str], level):
     total = 0
-    for line_id, line in enumerate(data):
+    for line in data:
         line, replacements = line.split(" ")
         replacements = [int(r) for r in replacements.split(",")]
         if level:
