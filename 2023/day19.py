2cf27d7ce91ad8fac9c49b206ccb8212df2d81a4
m <vogl91@gmail.com>
2023-12-19 09:50:41 +0100

    Day 19 recursive

--- 2023/day19.py
+++ 2023/day19.py
@@ -25,26 +25,23 @@ hdj{m>838:A,pv}
 {x=2461,m=1339,a=466,s=291}
 {x=2127,m=1623,a=2188,s=1013}""", 19114, 167409079868000)
 def test_19(data: List[str], level):
-    total = 0
     workflows, items = data
-    wf = {name: [Check(*p.split(":")) if ":" in p else Check(None, p) for p in rest.split(",")]
+    wf = {name: [Rule(*p.split(":")) if ":" in p else Rule(None, p) for p in rest.split(",")]
           for name, rest in (w.split("{") for w in workflows.split("\n"))}
     if level:
-        items_todo = [Item({k: range(1, 4001) for k in "xmas"}, "in", 0)]
-        while items_todo:
-            item = items_todo.pop()
-            if item.rule in "RA":
-                total += item.score()
-                continue
-            check = wf[item.rule][item.step]
-            items_todo.extend(item.split(check))
+        return work_b(Parts({k: range(1, 4001) for k in "xmas"}, "in", 0), wf)
     else:
         items = [{name: int(val) for part in line[1:-1].split(",") for name, val in [part.split("=")]}
                  for line in items.split("\n")]
-        return sum(work(item, wf) for item in items)
-    return total
+        return sum(work_a(item, wf) for item in items)
 
-def work(item, workflow, rule="in"):
+def work_b(parts, wf):
+    if parts.rule in "RA":
+        return parts.score()
+    check = wf[parts.rule][parts.step]
+    return sum(work_b(p, wf) for p in parts.split(check))
+
+def work_a(item, workflow, rule="in"):
     if rule == "A":
         return sum(item.values())
     elif rule == "R":
@@ -52,43 +49,9 @@ def work(item, workflow, rule="in"):
     for check in workflow[rule]:
         rule = check.perform_check(item)
         if rule is not None:
-            return work(item, workflow, rule)
+            return work_a(item, workflow, rule)
 
-@dataclass
-class Item:
-    val: Dict[str, range]
-    rule: str
-    step: int
-
-    def score(self):
-        return prod([len(self.val[i]) for i in "xmas"]) if self.rule == "A" else 0
-
-    def split(self, check: "Check") -> List["Item"]:
-        if check.var is None:
-            return [Item(self.val, check.target, 0)]
-
-        # check start and stop values
-        cond_start = check.perform_check_val(self.val[check.var].start)
-        cond_stop = check.perform_check_val(self.val[check.var].stop - 1)
-
-        # if both match, go to next rule
-        if cond_start and cond_stop:
-            return [Item(self.val, check.target, 0)]
-        # if none match, go to next step
-        elif not cond_start and not cond_stop:
-            return [Item(self.val, self.rule, self.step + 1)]
-
-        # the border of left/right is shifted by 1 in case of lt
-        cutoff = check.val if check.op == operator.lt else check.val + 1
-        # the check.var: statement overwrites the one field in the copied self.val dict
-        v0 = {**self.val, check.var: range(self.val[check.var].start, cutoff)}
-        v1 = {**self.val, check.var: range(cutoff, self.val[check.var].stop)}
-        # if the case only matches for the stop-value, swap the two
-        if cond_stop:
-            v0, v1 = v1, v0
-        return [Item(v0, check.target, 0), Item(v1, self.rule, self.step + 1)]
-
-class Check:
+class Rule:
     def __init__(self, var_to_check_op: Optional[str], target: str):
         self.target = target.replace("}", "")
         self.var = None
@@ -105,3 +68,37 @@ class Check:
     def perform_check_val(self, val: int) -> Optional[str]:
         if self.op(val, self.val):
             return self.target
+
+@dataclass
+class Parts:
+    val: Dict[str, range]
+    rule: str
+    step: int
+
+    def score(self):
+        return prod([len(self.val[i]) for i in "xmas"]) if self.rule == "A" else 0
+
+    def split(self, check: Rule) -> List["Parts"]:
+        if check.var is None:
+            return [Parts(self.val, check.target, 0)]
+
+        # check start and stop values
+        cond_start = check.perform_check_val(self.val[check.var].start)
+        cond_stop = check.perform_check_val(self.val[check.var].stop - 1)
+
+        # if both match, go to next rule
+        if cond_start and cond_stop:
+            return [Parts(self.val, check.target, 0)]
+        # if none match, go to next step
+        elif not cond_start and not cond_stop:
+            return [Parts(self.val, self.rule, self.step + 1)]
+
+        # the border of left/right is shifted by 1 in case of lt
+        cutoff = check.val if check.op == operator.lt else check.val + 1
+        # the check.var: statement overwrites the one field in the copied self.val dict
+        v0 = {**self.val, check.var: range(self.val[check.var].start, cutoff)}
+        v1 = {**self.val, check.var: range(cutoff, self.val[check.var].stop)}
+        # if the case only matches for the stop-value, swap the two
+        if cond_stop:
+            v0, v1 = v1, v0
+        return [Parts(v0, check.target, 0), Parts(v1, self.rule, self.step + 1)]
