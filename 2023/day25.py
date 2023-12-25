f8c3d54ca1e82ca1a1920af7da120dbbfbdec87a
m.vogl <markus.vogl@rise-world.com>
2023-12-25 06:23:12 +0100

    Day 25 with nx.minimum_edge_cut and connected_components

--- 2023/day25.py
+++ 2023/day25.py
@@ -1,8 +1,33 @@
 from typing import List
 
+import networkx as nx
 import pytest
+from matplotlib import pyplot as plt
 
-@pytest.mark.data("""TODO""", 1337, 1337)
-def test_25(data: List[str], level):
-    total = 0
-    return total
+
+@pytest.mark.data("""jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr""", 54, None)
+def test_25(data: List[str], level_a):
+    g = nx.Graph()
+    for line in data:
+        left, right = line.split(": ")
+        for right in right.split(" "):
+            g.add_edge(left, right)
+    nx.draw(g, with_labels=True)
+    plt.show()
+    g.remove_edges_from(nx.minimum_edge_cut(g))
+    nx.draw(g, with_labels=True)
+    plt.show()
+    cc = list(nx.connected_components(g))
+    return len(cc[0]) * len(cc[1])
