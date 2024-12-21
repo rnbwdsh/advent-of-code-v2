aba7b7bd2dfae3cdf3ad0f81f42faa16e1889b62
Markus <vogl91@gmail.com>
2024-12-21 17:17:17 +0100

    Refactored day21 in java

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,97 +1,73 @@
-import functools
-from typing import List, Tuple
+from functools import cache
+from itertools import takewhile
+from typing import List
 
 import pytest
 
 class Keypad:
     layout = ["789", "456", "123", " 0A"]
+
     def __init__(self, pos=3 + 2j, so_far=""):
         self.pos = pos
         self.so_far = so_far
 
-    def in_bounds(self):
-        return 0 <= self.pos.real < len(self.__class__.layout) and 0 <= self.pos.imag < len(self.__class__.layout[0]) and self.button_at() != " "
-
-    def button_at(self) -> str:
-        return self.__class__.layout[int(self.pos.real)][int(self.pos.imag)]
-
-    def solve(self, press: str) -> set[str]:
-        # otherwise, it tries to go up + down
+    def __call__(self, press: str) -> set[str]:
+        """ modified DFS with repetition action """
         if press == "A":
             return {"A"}
         curr = [self]
-        while press:
-            curr_next = []
-            success = 0
-            for c in curr:
-                for k, v in {'^': -1, 'v': 1, '<': -1j, '>': 1j}.items():
-                    cc = c.copy_move_keypad(k, v)
-                    if cc is None:
-                        continue
-                    if cc.button_at() == press[0]:
-                        success = 0
-                        for next_letter in press:
-                            if next_letter == press[0]:
-                                cc.so_far += "A"
-                                success += 1
-                            else:
-                                break
-                    curr_next.append(cc)
-            if success:
-                # hack: if you want to do the same char multiple times,
-                curr_next = [t for t in curr_next if t.so_far.endswith("A")]
-                press = press[success:]
+        while press and curr:
+            curr_next, found = self.find_inner(curr, press)
+            if found:
+                curr_next = [c for c in curr_next if c.so_far.endswith("A")]
+                press = press[found:]
             curr = curr_next
         return set([c.so_far for c in curr])
 
-    def copy_move_keypad(self, letter, direction):
-        c = self.__class__(self.pos, self.so_far)
-        c.pos += direction
-        c.so_far += letter
-        if not c.in_bounds():
-            return None
-        return c
+    def find_inner(self, curr, press):
+        dir_items = {'^': -1, 'v': 1, '<': -1j, '>': 1j}.items()
+        curr_next = []
+        found = 0
+        for k, v in dir_items:
+            for c in curr:
+                cpy = self.__class__(c.pos + v, c.so_far + k)
+                if cpy.button_at() == ' ':
+                    continue
+                if cpy.button_at() == press[0]:
+                    # if the same button has to be pressed multiple times, we just repeatedly press A
+                    found = sum(1 for _ in takewhile(lambda x: x == press[0], press))
+                    cpy.so_far += "A" * found
+                curr_next.append(cpy)
+        return curr_next, found
 
+    def button_at(self) -> str:
+        row, col = int(self.pos.real), int(self.pos.imag)
+        if 0 <= row < len(self.layout) and 0 <= col < len(self.layout[0]):
+            return self.layout[row][col]
+        return " "
 
 class Dpad(Keypad):
     layout = [" ^A", "<v>"]
-    def __init__(self, pos = 2j, so_far =""):
+
+    def __init__(self, pos=2j, so_far=""):
         super().__init__(pos, so_far)
 
-
-@functools.cache
-def dpad_cached(s: str) -> set[str]:
-    assert s[-1] == "A"
-    return Dpad().solve(s)
-
-def yield_chunks(s: str) -> Tuple[str, str]:
-    """ Split every keypad action into sub-actions that end with A"""
-    while s:
-        end = s.find("A") + 1
-        yield s[:end]
-        s = s[end:]
-
-@functools.cache
-def tree_replace_resolve(s: str, depth = 2) -> List | int:
+@cache
+def tree_fold(s: str, depth) -> List | int:
+    """ build and consume a recursive substitution tree to find the shortest substitution """
     if depth == 0:
         return len(s)
-    return sum([min([tree_replace_resolve(chunk, depth - 1)
-                     for chunk in dpad_cached(chunk)])
-                for chunk in yield_chunks(s)])
+    return sum(min(tree_fold(solved_chunk, depth - 1) for solved_chunk in Dpad()(chunk + "A"))
+               for chunk in s.split("A")[:-1])
 
-@pytest.mark.data("""029A
-980A
-179A
-456A
-379A""", 126384, None)
+@pytest.mark.data("029A\n980A\n179A\n456A\n379A", 126384, None)
 def test_20(data: List[str], level):
     repeat = 25 if level else 2
-    return sum(int(line[:3]) * min(tree_replace_resolve(k, repeat)
-                                   for k in Keypad().solve(line))
+    return sum(int(line[:3]) * min(tree_fold(k, repeat) for k in Keypad()(line))
                for line in data)
 
-def test___tree():
+def test_keypad_dpad_tree():
     example = "<A^A>^^AvvvA"
-    assert Keypad().solve("029A") == {example, "<A^A^>^AvvvA", "<A^A^^>AvvvA"}
-    assert "v<<A>>^A<A>AvA<^AA>A<vAAA>^A" in Dpad().solve(example)
-    assert tree_replace_resolve(example, 2) == 68
+    assert Keypad().__call__("029A") == {example, "<A^A^>^AvvvA", "<A^A^^>AvvvA"}
+    assert "v<<A>>^A<A>AvA<^AA>A<vAAA>^A" in Dpad()(example)
+    assert tree_fold(example, 2) == 68
