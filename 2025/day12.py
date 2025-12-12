ceeda851c0296bb65b48b5aadce155df3f24fff2
rnbwdsh <vogl91@gmail.com>
2025-12-12 10:18:00 +0100

    code cleanup

--- 2025/day12.py
+++ 2025/day12.py
@@ -4,12 +4,6 @@ import numpy as np
 import pytest
 
 
-def check(area, counts, shapes) -> int:
-    area_a = area[0] * area[1]
-    space_per_shape = np.array([s.sum() for s in shapes])
-    return sum(space_per_shape * counts) < area_a * 0.85
-
-
 @pytest.mark.data('''0:
 ###
 ##.
@@ -51,6 +45,5 @@ def test_12(data: List[List[str]], level):
     for line in data[-1]:
         area, *counts = line.split(' ')
         area = [int(i) for i in area[:-1].split("x")]
-        counts = np.array([int(c) for c in counts])
-        total += check(area, counts, shapes)
+        total += sum(s.sum() * int(c) for s, c in zip(shapes, counts)) <= area[0] * area[1] * 0.85
     return total
